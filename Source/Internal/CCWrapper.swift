be78b49d70506a227ad3def77e9396a62b684fd3
Ruben Nine <ruben@9labs.io>
2019-09-20 00:15:00 +0200

    Misc cleanup.

--- Source/Internal/CCWrapper.swift
+++ Source/Internal/CCWrapper.swift
@@ -16,10 +16,6 @@ internal final class CCWrapper {
     public let algorithm: DigestAlgorithm
 
     private var digest: UnsafeMutablePointer<UInt8>?
-    private var md5Ctx: CC_MD5_CTX?
-    private var sha1Ctx: CC_SHA1_CTX?
-    private var sha256Ctx: CC_SHA256_CTX?
-    private var sha512Ctx: CC_SHA512_CTX?
     private var updateFun: CC_XXX_Update?
     private var finalFun: CC_XXX_Final?
 
@@ -29,55 +25,32 @@ internal final class CCWrapper {
         switch algorithm {
         case .md5:
             var ctx = CC_MD5_CTX()
-
             CC_MD5_Init(&ctx)
-
-            md5Ctx = ctx
             updateFun = { data, len in CC_MD5_Update(&ctx, data, len) }
             finalFun = { digest in CC_MD5_Final(digest, &ctx) }
-
         case .sha1:
             var ctx = CC_SHA1_CTX()
-
             CC_SHA1_Init(&ctx)
-
-            sha1Ctx = ctx
             updateFun = { data, len in CC_SHA1_Update(&ctx, data, len) }
             finalFun = { digest in CC_SHA1_Final(digest, &ctx) }
-
         case .sha224:
             var ctx = CC_SHA256_CTX()
-
             CC_SHA224_Init(&ctx)
-
-            sha256Ctx = ctx
             updateFun = { data, len in CC_SHA224_Update(&ctx, data, len) }
             finalFun = { digest in CC_SHA224_Final(digest, &ctx) }
-
         case .sha256:
             var ctx = CC_SHA256_CTX()
-
             CC_SHA256_Init(&ctx)
-
-            sha256Ctx = ctx
             updateFun = { data, len in CC_SHA256_Update(&ctx, data, len) }
             finalFun = { digest in CC_SHA256_Final(digest, &ctx) }
-
         case .sha384:
             var ctx = CC_SHA512_CTX()
-
             CC_SHA384_Init(&ctx)
-
-            sha512Ctx = ctx
             updateFun = { data, len in CC_SHA384_Update(&ctx, data, len) }
             finalFun = { digest in CC_SHA384_Final(digest, &ctx) }
-
         case .sha512:
             var ctx = CC_SHA512_CTX()
-
             CC_SHA512_Init(&ctx)
-
-            sha512Ctx = ctx
             updateFun = { data, len in CC_SHA512_Update(&ctx, data, len) }
             finalFun = { digest in CC_SHA512_Final(digest, &ctx) }
         }
@@ -107,12 +80,8 @@ internal final class CCWrapper {
         // We DON'T have a digest YET, return early
         guard let digest = digest else { return nil }
 
-        var string = ""
-
-        for i in 0 ..< algorithm.digestLength {
-            string += String(format: "%02x", digest[i])
+        return (0..<algorithm.digestLength).reduce(into: "") { (str, i) in
+            str += String(format: "%02x", digest[i])
         }
-
-        return string
     }
 }
